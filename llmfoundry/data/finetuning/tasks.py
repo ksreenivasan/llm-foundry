4c9b290a016e836570a312bbc659d426f185ba73
Kartik Sreenivasan <kartik.sreenivasan@databricks.com>
2024-02-06 15:40:25 -0800

    adding metamathqa preprocessing function

--- llmfoundry/data/finetuning/tasks.py
+++ llmfoundry/data/finetuning/tasks.py
@@ -654,7 +654,7 @@ def dolly_preprocessing_function(inp: Dict) -> Dict[str, str]:
 @dataset_constructor.register('gsm8k')
 def gsm8k_preprocessing_function(inp: Dict) -> Dict[str, str]:
     """Format the text string."""
-    PROMPT_FORMAT = 'Below is a math problem. Let\'s think step by step, solve it carefully and output the answer. Make sure to use"#### as a delimiter to separate your thinking and the final answer.\n\n#### Q:\n{question}\n\n#### Answer:\n'
+    PROMPT_FORMAT = 'Below is a math problem. Let\'s think step by step, solve it carefully and output the answer. Make sure to use #### as a delimiter to separate your thinking and the final answer.\n\n#### Q:\n{question}\n\n#### Answer:\n'
     try:
         question = inp['question']
         prompt = PROMPT_FORMAT.format(question=question)
@@ -664,7 +664,18 @@ def gsm8k_preprocessing_function(inp: Dict) -> Dict[str, str]:
             f'Unable to extract prompt/response from {inp=}') from e
     return {'prompt': prompt, 'response': answer}
 
-
+@dataset_constructor.register('meta-math/MetaMathQA')
+def metamath_preprocessing_function(inp: Dict) -> Dict[str, str]:
+    """Format the text string."""
+    PROMPT_FORMAT = 'Below is a math problem. Let\'s think step by step, solve it carefully and output the answer. Make sure to use #### as a delimiter to separate your thinking and the final answer.\n\n#### Q:\n{question}\n\n#### Answer:\n'
+    try:
+        question = inp['query']
+        prompt = PROMPT_FORMAT.format(question=question)
+        answer = inp['response']
+    except Exception as e:
+        raise ValueError(
+            f'Unable to extract prompt/response from {inp=}') from e
+    return {'prompt': prompt, 'response': answer}
 
 @dataset_constructor.register('bigscience/P3')
 def p3_preprocessing_function(inp: Dict) -> Dict[str, str]:
